678b96919d8086e7ba0a10b853ec3efb63b7c250
Adina Matei <adinamateim@gmail.com>
2017-10-16 16:46:13 +0300

    added ShowCard component

--- React-Workshop/js/Search.jsx
+++ React-Workshop/js/Search.jsx
@@ -1,11 +1,12 @@
 import React  from 'react';
+import ShowCard from './ShowCard'
 import preload from '../data.json';
 
 const Search = () => (
   <div className="search">
     {/* print data.json */}
     {/* <pre><code>{JSON.stringify(preload, null, 4)}</code></pre> */}
-    {preload.shows.map(show => <h3>{show.title}</h3> )}
+    {preload.shows.map(show => <ShowCard show={show} />)}
   </div>
 );
 
